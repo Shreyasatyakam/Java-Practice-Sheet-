f2bb1f845be863768dc168f35aebb7b38333170e
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2025-09-03 22:07:51 +0530

    Create MergeSortedArrays.java

---
+++ MergeSortedArrays.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input first array
+        System.out.print("Enter size of first sorted array: ");
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        System.out.println("Enter elements of first sorted array:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        // Input second array
+        System.out.print("Enter size of second sorted array: ");
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        System.out.println("Enter elements of second sorted array:");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        // Merge arrays
+        int[] merged = merge(arr1, arr2);
+
+        // Print result
+        System.out.println("Merged Sorted Array: " + Arrays.toString(merged));
+        sc.close();
+    }
+
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int n1 = arr1.length, n2 = arr2.length;
+        int[] result = new int[n1 + n2];
+        int i = 0, j = 0, k = 0;
+
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < n1) {
+            result[k++] = arr1[i++];
+        }
+        while (j < n2) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+}
