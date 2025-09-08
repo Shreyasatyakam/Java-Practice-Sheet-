056239428e2a9c20cb8a94d59c1d1a8e98058c79
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2025-09-08 11:29:32 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,14 +8,14 @@ public class BubbleSort {
             swapped = false;
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap arr[j] and arr[j+1]
+                    
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
-            // If no elements were swapped, array is already sorted
+          
             if (!swapped) break;
         }
     }
@@ -23,15 +23,12 @@ public class BubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input array
         System.out.print("Enter numbers separated by space: ");
         String[] parts = sc.nextLine().split("\\s+");
         int[] arr = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
-
-        // Sort array
+        
         bubbleSort(arr);
 
-        // Print sorted array
         System.out.println("Sorted array: " + Arrays.toString(arr));
 
         sc.close();
