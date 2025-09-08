7e146fdc53021244061f6525d7fb6022c0645df1
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2025-09-08 12:29:40 +0530

    Update FibonacciDemo.java

--- FibonacciDemo.java
+++ FibonacciDemo.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class FibonacciDemo {
 
-    //  Naive Recursion
     static int fibRecursive(int n) {
         if (n <= 1) return n;
         return fibRecursive(n - 1) + fibRecursive(n - 2);
     }
 
-    // Memoization (Top-Down DP)
+
     static int fibMemo(int n, Map<Integer, Integer> memo) {
         if (n <= 1) return n;
         if (memo.containsKey(n)) return memo.get(n);
@@ -24,12 +23,11 @@ public class FibonacciDemo {
         System.out.print("Enter n (Fibonacci term): ");
         int n = sc.nextInt();
 
-        // Recursive approach
         long start1 = System.nanoTime();
         int result1 = fibRecursive(n);
         long end1 = System.nanoTime();
 
-        // Memoized approach
+
         long start2 = System.nanoTime();
         int result2 = fibMemo(n, new HashMap<>());
         long end2 = System.nanoTime();
